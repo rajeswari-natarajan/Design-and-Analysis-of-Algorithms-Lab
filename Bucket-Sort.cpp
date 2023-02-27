5da5a9a14f60db8ab1a1dbcdf48978d5514efce9
Rajeswari Natarajan <47252119+rajeswari-natarajan@users.noreply.github.com>
2023-02-27 15:08:31 +0530

    Update Bucket-Sort.cpp

--- Bucket-Sort.cpp
+++ Bucket-Sort.cpp
@@ -1,6 +1,6 @@
 # include <iostream>
 # define max 10
-# define inf -1000
+# define inf 1000
 using namespace std;
 float a[max],n;
 float s[max];
@@ -13,7 +13,7 @@ struct node *b[max];
 void print()
 {
 	int i;
-	for (i=0;i<n;i++)
+	for (i=1;i<=n;i++)
 		cout<<a[i]<<"\t";
 	cout<<endl;
 }
@@ -24,25 +24,24 @@ void read()
 	cout<<"No of inputs :";
 	cin>>n;
 	cout<<"Values ?\n";
-	for (i=0;i<n;i++)
+	for (i=1;i<=n;i++)
 		cin>>a[i];
 		
 }
 void printB()
 {
 	struct node *t;
-	int p,i,v=0;
+	int p,i,j=1;
 	for (i=0;i<n;i++)
 	{
 		if (b[i]!=NULL)
 		{
 			cout<<"\nList["<<i<<"]\n";
-		
 			for (t=b[i];t!=NULL;t=t->next)
-			{
-				a[v]=t->val;
+			{	
+				a[j]=t->val;
+				j++;
 				cout<<t->val<<"\t";
-				v++;
 			}
 		}
 	}
@@ -53,29 +52,22 @@ void insertionSort(float s[], int n1)
 {
 	int i, j;
 	float key;
-	for (i = 1; i < n1; i++)
+	for (i = 1; i <= n1; i++)
 	{
 		key = s[i];
 		j = i - 1;
-
-		// Move elements of arr[0..i-1],
-		// that are greater than key, to one
-		// position ahead of their
-		// current position
 		while (j >= 0 && s[j] > key)
 		{
 			s[j + 1] = s[j];
 			j = j - 1;
-			//s++;
-			//cout<<j<<"\t"<<s<<"\n";
 		}
 		s[j + 1] = key;
 	}
 }
 void clearArray()
 {
-	for (int i=0;i<max;i++)
-		s[i]=-1;
+	for (int i=1;i<=max;i++)
+		s[i]=inf;
 }
 
 
@@ -83,16 +75,17 @@ void bucSort()
 {
 	
 	struct node *temp, *curr;
-	int i,j;
+	int i,j,p;
 	for (i=0;i<n;i++)
 		b[i]=NULL;
-	for (i=0;i<n;i++)	
+	for (i=1;i<=n;i++)	
 	{
 		temp=new node;
 		temp->val= a[i];
 		temp->next = NULL;
-		int p;
-		p=n * a[i];
+		p= n * a[i];
+		cout<<p<<endl;
+		
 		if (b[p] == NULL)
 			b[p]=temp;
 		else
@@ -101,23 +94,23 @@ void bucSort()
 				curr->next=temp;
 			}
 	}
+	cout<<"Before sorting \n";
 	//copy elements of b[i]  in s[] for insertion sort
 	struct node *t;
-	int tot;
+	int tot,k;
 	for (i=0;i<n;i++)
 	{
 		if (b[i]!=NULL)
 		{
-			for (t=b[i],j=0;t!=NULL;t=t->next,j++)
+			for (t=b[i],j=1;t!=NULL;t=t->next,++j)
 				s[j]=t->val;
-				//cout<<t->val<<"\t";
-			tot=j;
+			tot=j-1;
 			insertionSort(s,tot);
-			for (t=b[i],j=0;t!=NULL;t=t->next,j++)
+			for (t=b[i],j=1;t!=NULL;t=t->next,j++)
 				t->val=s[j];
-			
 			clearArray();
-		}	
+		}
+
 	}
 	printB();
 }
